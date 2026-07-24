2616843b32c4101737c9310afd5abe437288ba17
TenzDelek <tibetdelek@gmail.com>
2026-07-24 11:49:12 +0530

    base

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,26 @@
 import { AppToastHost } from '@/components/common/AppToastHost';
-import { ForceUpdateGate } from '@/components/ForceUpdateGate';
 import { AppColors } from '@/constants/app-colors';
-import { ensureI18nReady } from '@/lib/i18n';
+import localeEn from "@/i18n/en.json";
 import { configureNotificationHandler } from '@/lib/notifications';
 import { AuthTokenSync } from '@/providers/auth-token';
 import { Auth0ProviderWrapper } from '@/providers/auth0';
 import { GuestProvider, useGuest } from '@/providers/guest';
 import { MalaSyncBootstrap } from '@/providers/mala-sync';
-import { PendingOnboardingPlanProvider } from '@/providers/pending-onboarding-plan';
-import { PendingNotificationNavProvider } from '@/providers/pending-notification-nav';
 import { NotificationSyncBootstrap } from '@/providers/notification-sync';
 import { OnboardingProvider, useOnboarding } from '@/providers/onboarding';
+import { PendingNotificationNavProvider } from '@/providers/pending-notification-nav';
+import { PendingOnboardingPlanProvider } from '@/providers/pending-onboarding-plan';
 import { QueryProvider } from '@/providers/query';
 import { ThemeProvider } from '@/providers/theme';
+import { DEFAULT_LANGUAGE, LANGUAGE } from "@/utils/constant";
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import {
+  BackendFetch,
+  DevTools,
+  FormatSimple,
+  Tolgee,
+  TolgeeProvider,
+} from "@tolgee/react";
 import { useFonts } from 'expo-font';
 import {
   Stack,
@@ -25,9 +32,33 @@ import {
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useAuth0 } from 'react-native-auth0';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../../global.css';
+
+
+if (!localStorage.getItem(LANGUAGE)) {
+  localStorage.setItem(LANGUAGE, DEFAULT_LANGUAGE);
+}
+
+const tolgee = Tolgee()
+  .use(DevTools())
+  .use(FormatSimple())
+  // replace with .use(FormatIcu()) for rendering plurals, formatted numbers, etc.
+  .use(
+    BackendFetch({
+      prefix:
+        "https://cdn.tolg.ee/a23495c159b886551292e856ecf7a332/webuddhist",
+      fallbackOnFail: true,
+    }),
+  )
+  .init({
+    language: localStorage.getItem(LANGUAGE) || DEFAULT_LANGUAGE,
+    fallbackLanguage: "en",
+    staticData: {
+      en: async () => localeEn,
+    },
+  });
 
 SplashScreen.preventAutoHideAsync();
 configureNotificationHandler();
@@ -127,10 +158,6 @@ export default function RootLayout() {
   const [i18nReady, setI18nReady] = useState(false);
 
   useEffect(() => {
-    ensureI18nReady().then(() => setI18nReady(true));
-  }, []);
-
-  useEffect(() => {
     if ((fontsLoaded || fontError) && i18nReady) {
       SplashScreen.hideAsync();
     }
@@ -141,26 +168,28 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <QueryProvider>
-        <Auth0ProviderWrapper>
-          <GuestProvider>
-            <AuthTokenSync>
-              <PendingNotificationNavProvider>
-                <NotificationSyncBootstrap />
-                <PendingOnboardingPlanProvider>
-                  <MalaSyncBootstrap />
-                  <OnboardingProvider>
-                    <ThemeProvider>
-                      <BottomSheetModalProvider>
-                        <AuthGate />
-                        <AppToastHost />
-                      </BottomSheetModalProvider>
-                    </ThemeProvider>
-                  </OnboardingProvider>
-                </PendingOnboardingPlanProvider>
-              </PendingNotificationNavProvider>
-            </AuthTokenSync>
-          </GuestProvider>
-        </Auth0ProviderWrapper>
+        <TolgeeProvider tolgee={tolgee}>
+          <Auth0ProviderWrapper>
+            <GuestProvider>
+              <AuthTokenSync>
+                <PendingNotificationNavProvider>
+                  <NotificationSyncBootstrap />
+                  <PendingOnboardingPlanProvider>
+                    <MalaSyncBootstrap />
+                    <OnboardingProvider>
+                      <ThemeProvider>
+                        <BottomSheetModalProvider>
+                          <AuthGate />
+                          <AppToastHost />
+                        </BottomSheetModalProvider>
+                      </ThemeProvider>
+                    </OnboardingProvider>
+                  </PendingOnboardingPlanProvider>
+                </PendingNotificationNavProvider>
+              </AuthTokenSync>
+            </GuestProvider>
+          </Auth0ProviderWrapper>
+        </TolgeeProvider>
       </QueryProvider>
     </GestureHandlerRootView>
   );
